3ecd7c694272f86c370b796b88645c8423b413f3
Evgeniy Siganov <EvgeniySiganov>
2024-11-08 22:23:29 +0300

    init

--- src/main/java/ru/iteco/accountbank/AccountBankApplication.java
+++ src/main/java/ru/iteco/accountbank/AccountBankApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import ru.iteco.accountbank.model.AccountInfo;
 import ru.iteco.accountbank.service.*;
+import ru.iteco.accountbank.service.Process;
 
 @ComponentScan
 @PropertySource("classpath:application.properties")
@@ -13,14 +14,27 @@ public class AccountBankApplication {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AccountBankApplication.class);
-        AccountService accountService = applicationContext.getBean(AccountService.class);
-        System.out.println("Personal info class: " + accountService.getPersonalInfoClass());
-        AccountInfo accountInfoById = accountService.getAccountInfoById(1);
-        System.out.println("Personal info class: " + accountService.getPersonalInfoClass());
-        System.out.println(accountInfoById);
+//        AccountService accountService = applicationContext.getBean(AccountService.class);
+//        System.out.println("Personal info class: " + accountService.getPersonalInfoClass());
+//        AccountInfo accountInfoById = accountService.getAccountInfoById(1);
+//        System.out.println("Personal info class: " + accountService.getPersonalInfoClass());
+//        System.out.println(accountInfoById);
+//
+//        IObject bean = applicationContext.getBean(IObject.class);
+//        System.out.println("object type is: " + bean.getClass());
+//        System.out.println("result info is: " + bean.getInfo());
 
-        IObject bean = applicationContext.getBean(IObject.class);
-        System.out.println("object type is: " + bean.getClass());
-        System.out.println("result info is: " + bean.getInfo());
+//        Flow flow = applicationContext.getBean(Flow.class);
+//
+//        flow.run(1);
+//
+//        flow.run(2);
+//
+//        flow.run(2);
+//
+//        flow.run(3);
+//
+//        flow.run(4);
+//        flow.run(4);
     }
 }
